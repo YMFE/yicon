fea4d431c23797c317d0dbd8bf623f86d4be9fb6
malcolm <minghao_yu@163.com>
2016-07-04 15:15:49 +0800

    修正全大库接口

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,15 @@ module.exports = function (config) {
         loaders: [
           { test: /\.(jpe?g|png|gif|svg)$/, loader: 'url', query: {limit: 10240} },
           { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel']},
-          { test: /\.scss$/, loader: 'style!css?modules&importLoaders=2&sourceMap&localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap' }
+          { test: /\.scss$/, loader: [
+            'style',
+            'css' +
+              '?modules' +
+              '&localIdentName=[path][name]-[local]',
+            'postcss',
+            'sass' +
+              '?outputStyle=expanded',
+          ] },
         ]
       },
       resolve: {
