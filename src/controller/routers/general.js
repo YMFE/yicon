4155cb33c880855fb9e6deed3426c4bf885a1e0f
jason fang <fangcq_1993@163.com>
2016-07-05 14:42:57 +0800

    图标搜索功能

--- src/controller/routers/general.js
+++ src/controller/routers/general.js
@@ -1,11 +1,12 @@
 import Router from 'koa-router';
 import { getOne, list } from '../modules/repository';
-import { getById } from '../modules/icon';
+import { getById, getByCondition } from '../modules/icon';
 
 const general = new Router();
 
 general.get('/repositories', list);
 general.get('/repositories/:repoId', getOne);
 general.post('/icons', getById);
+general.get('/icons', getByCondition);
 
 export default general;
