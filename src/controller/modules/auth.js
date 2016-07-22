72d17882f60ad3176efc688e5615763c3ae89234
jason fang <fangcq_1993@163.com>
2016-07-22 13:21:13 +0800

    登录

---
+++ src/controller/modules/auth.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+import { User } from '../../model';
+
+function* verifyToken(token) {
+  const verifyTokenURL = `http://qsso.corp.qunar.com/api/verifytoken.php?token=${token}`;
+  return yield axios.get(verifyTokenURL)
+    .then(value => value.data)
+    .catch(err => {
+      throw new Error(err);
+    });
+}
+
+function* insertToDB(data) {
+  const { userId } = data;
+  const info = yield User.findOrCreate({
+    where: { name: userId },
+    defaults: {
+      name: userId,
+      actor: 0,
+    },
+  });
+  return info;
+}
+
+export function* getUserInfo(next) {
+  const { token } = this.param;
+  const verifyResult = yield verifyToken(token);
+  if (verifyResult.ret) {
+    const info = yield insertToDB(verifyResult.data);
+    this.session.name = encodeURIComponent(verifyResult.data.userInfo.name);
+    this.session.userId = info[0].id;
+    this.session.domain = info[0].name;
+    this.session.actor = info[0].actor;
+    this.redirect('/');
+  } else {
+    throw new Error(verifyResult.errmsg);
+  }
+  yield next;
+}
+
+export function* isLogin(next) {
+  const session = this.session;
+  if (!session || !session.userId) throw new Error('尚未登录');
+  yield next;
+}
+
+export function* clearUserInfo(next) {
+  this.session = null;
+  this.redirect('/');
+  yield next;
+}
