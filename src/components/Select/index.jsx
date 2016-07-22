7de06537427f6e651221d9ffe0a9a0c42673b06c
wanglili <wang508x102@163.com>
2016-07-22 17:16:34 +0800

    update select

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -1,56 +1,65 @@
-import './Select.scss';
 import React, { Component, PropTypes } from 'react';
 import RcSelect, { Option, OptGroup } from 'rc-select';
-import classNames from 'classnames';
-export default class Select extends Component {
+// import '../../rc-select/assets/index.css';
+import './Select.scss';
+// console.log('Option',Option);
+/**
+ * 评分组件
+ *
+ * @component Selectfield
+ * @version  0.0.1
+ * @description 模拟select。
+ * @autor leila.wang
+ */
+
+export default class Selectfield extends Component {
+    //  es7  写法
   static Option = Option;
   static OptGroup = OptGroup;
 
   static defaultProps = {
-    prefixCls: 'yiconselect',
+    extraClass: '',
     transitionName: 'slide-up',
     choiceTransitionName: 'zoom',
     showSearch: false,
-  }
-  static propTypes = {
-    className: PropTypes.string,
-    optionLabelProp: PropTypes.string,
+    notFoundContent: 'Not Found',
   }
   static contextTypes = {
     antLocale: React.PropTypes.object,
   }
-
+  static propTypes = {
+    extraClass: PropTypes.string,
+    prefixCls: PropTypes.string,
+    showSearch: PropTypes.bool,
+    optionLabelProp: PropTypes.string,
+    notFoundContent: PropTypes.string,
+    combobox: PropTypes.string,
+  }
   render() {
-    const {
-      className,
-    } = this.props;
+    const { extraClass } = this.props;
+    const selectClass = ['rc-select', extraClass].join(' ');
+    // let {
+    //   size, className, combobox, notFoundContent, prefixCls, showSearch, optionLabelProp,
+    // } = this.props;
+    let { notFoundContent, optionLabelProp } = this.props;
+    const { combobox } = this.props;
+    const { antLocale } = this.context;
+    if (antLocale && antLocale.Select) {
+      notFoundContent = notFoundContent || antLocale.Select.notFoundContent;
+    }
 
-    const cls = classNames({
-      // [`${prefixCls}-lg`]: size === 'large',
-      // [`${prefixCls}-sm`]: size === 'small',
-      [className]: !!className,
-    });
+    if (combobox) {
+      notFoundContent = null;
+      // children 带 dom 结构时，无法填入输入框
+      optionLabelProp = optionLabelProp || 'value';
+    }
+
     return (
-      <RcSelect className={cls} />
+      <RcSelect
+        {...this.props} className={selectClass}
+        optionLabelProp={optionLabelProp || 'children'}
+        notFoundContent={notFoundContent}
+      />
     );
-    /*
-      // const { antLocale } = this.context;
-      // if (antLocale && antLocale.Select) {
-      //   notFoundContent = notFoundContent || antLocale.Select.notFoundContent;
-      // }
-
-      // if (combobox) {
-      //   notFoundContent = null;
-      //   // children 带 dom 结构时，无法填入输入框
-      //   optionLabelProp = optionLabelProp || 'value';
-      // }
-    */
-    // return (
-    //   <RcSelect
-    //     {...this.props}
-    //     className={cls}
-    //     optionLabelProp={optionLabelProp || 'children'}
-    //   />
-    // );
   }
 }
