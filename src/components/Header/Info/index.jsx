39139292c5155661ac961452c79e43f653691b90
xilixinluo <xilixinluo@gmail.com>
2016-08-02 21:54:21 +0800

    fixed header info

--- src/components/Header/Info/index.jsx
+++ src/components/Header/Info/index.jsx
@@ -6,7 +6,11 @@ const Info = (props) => (
   <li className="lists">
     <Link to="/user/notifications" className="nav-message">
       <i className="iconfont">&#xf50d;</i>
-      <i className="nav-message-count">{props.infoCont}</i>
+      {
+        props.infoCont
+        ? <i className="nav-message-count">{props.infoCont}</i>
+        : null
+      }
     </Link>
   </li>
 );
