73ea78ef675bfcfc70c197bd8b6acf085e5dbe6e
wanglili <wang508x102@163.com>
2016-08-02 13:15:03 +0800

    add slick component

--- src/components/common/Slick/index.jsx
+++ src/components/common/Slick/index.jsx
@@ -1,76 +1,136 @@
 import './Slick.scss';
 import React, { Component, PropTypes } from 'react';
-const itemData = [
-  {
+const itemData = [];
+for (let i = 0; i < 28; i++) {
+  const obj = {
+    index: i,
     name: '笔',
     tags: '你好,呵呵',
     code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  },
-  {
-    name: '笔',
-    tags: '你好,呵呵',
-    code: 61442,
-  }];
+  };
+  itemData.push(obj);
+}
+// const itemData = [
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   },
+//   {
+//     name: '笔',
+//     tags: '你好,呵呵',
+//     code: 61442,
+//   }];
 
 // itemDate 为icon数据 存入state中 方便操作删除当前后 重新渲染
 export default class Slick extends Component {
@@ -82,7 +142,8 @@ export default class Slick extends Component {
     iconItemListPos: {
       transform: 'translateX(0)',
     },
-    leftStep: 84,
+    // leftStep: 84 * 12,
+    leftStep: 1008,
   }
   static propTypes = {
     currentItem: PropTypes.number,
@@ -100,33 +161,34 @@ export default class Slick extends Component {
       currentItem: this.props.defaultCurrent,
       defaultTranslateX: this.props.defaultTranslateX,
       iconItemListPos: Object.assign({}, this.props.iconItemListPos),
+      scrollAreaWidth: 0,
     //  itemData: this.props.itemData,
     };
   }
-  // componentWillMount() {
-  //   this.setState({
-  //     itemData: itemData,
-  //   });
-  // }
+  componentWillMount() {
+    const uiWidth = 84 * itemData.length;
+    // 删除的时候 需要重新渲染 itemData: itemData,
+    this.setState({ scrollAreaWidth: uiWidth });
+  }
   handleClick(config, evt) {
     const { type, index } = config;
-    let _currentItem = this.state.currentItem;
-    let _iconItemListPosLeft = this.state.defaultTranslateX;
+    const { currentItem, defaultTranslateX, scrollAreaWidth } = this.state;
+    let _currentItem = currentItem;
+    let _iconItemListPosLeft = defaultTranslateX;
     switch (type) {
       case 'item':
         _currentItem = index;
         break;
       case 'prev':
-        // if (this.state.currentItem > 1) {
-        //   _currentItem = this.state.currentItem - 1;
-        // }
         console.log('上一页');
         _iconItemListPosLeft += this.props.leftStep;
-        this.setState({ defaultTranslateX: _iconItemListPosLeft });
+        if (_iconItemListPosLeft <= 0) {
+          this.setState({ defaultTranslateX: _iconItemListPosLeft });
+        }
         console.log(_iconItemListPosLeft);
-        this.setState({ iconItemListPos: {
-          transform: `translateX(${_iconItemListPosLeft}px)`,
-        } });
+        // this.setState({ iconItemListPos: {
+        //   transform: `translateX(${_iconItemListPosLeft}px)`,
+        // } });
         break;
       case 'next':
         // if (this.state.currentPage < this.props.totalPage) {
@@ -135,11 +197,15 @@ export default class Slick extends Component {
         evt.preventDefault();
         console.log('下一页');
         _iconItemListPosLeft -= this.props.leftStep;
-        console.log(_iconItemListPosLeft);
-        this.setState({ defaultTranslateX: _iconItemListPosLeft });
-        this.setState({ iconItemListPos: {
-          transform: `translateX(${_iconItemListPosLeft}px)`,
-        } });
+        console.log(scrollAreaWidth);
+        if (Math.abs(_iconItemListPosLeft) < scrollAreaWidth) {
+          this.setState({ defaultTranslateX: _iconItemListPosLeft });
+        }
+
+        // this.setState({ defaultTranslateX: _iconItemListPosLeft });
+        // this.setState({ iconItemListPos: {
+        //   transform: `translateX(${_iconItemListPosLeft}px)`,
+        // } });
         break;
       default:
         break;
@@ -163,10 +229,12 @@ export default class Slick extends Component {
     console.log(index);
     console.log(evt);
     alert('delete');
+    // 重新计算滚动ul 宽
   }
   render() {
     const itemArr = [];
-    const { currentItem, iconItemListPos } = this.state;
+    // iconItemListPos
+    const { currentItem, defaultTranslateX, scrollAreaWidth } = this.state;
     itemData.forEach((item, i) => {
       itemArr.push(<li
         key={`item_${i}`}
@@ -177,7 +245,7 @@ export default class Slick extends Component {
           className={'iconfont delete'}
           onClick={(evt) => this.deleteSingleClick({ index: i }, evt)}
         >&#xf077;</i>
-        <i className={'iconfont upload-icon'}>&#xf50f;</i>
+        <i className={'iconfont upload-icon'}>{i}</i>
         {/* &#xf50f; {`&#${item.code};`} */}
       </li>);
     });
@@ -189,7 +257,11 @@ export default class Slick extends Component {
         >
           <i className={'iconfont icons-more-btn-icon'}>&#xf1c3;</i></button>
         <div className={'upload-icon-list-area'}>
-          <ul ref={'uploadIconList'} className={'upload-icon-list'} style={iconItemListPos}>
+          <ul
+            ref={'uploadIconList'}
+            className={'upload-icon-list'}
+            style={{ marginLeft: defaultTranslateX, width: scrollAreaWidth }}
+          >
             {itemArr}
           </ul>
         </div>
