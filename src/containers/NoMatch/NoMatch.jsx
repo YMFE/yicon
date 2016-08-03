2b06a5d3c5c2364b900b224028904a91be43ea75
wanglili <wang508x102@163.com>
2016-08-03 15:55:13 +0800

    nomatch

--- src/containers/NoMatch/NoMatch.jsx
+++ src/containers/NoMatch/NoMatch.jsx
@@ -3,8 +3,12 @@ import './NoMatch.scss';
 export default class NoMatch extends Component {
   render() {
     return (
-      <div className={'NoMatch'}>
-
+      <div className={'no-match'}>
+        <div className={'no-match-logo'}></div>
+        <div className={'no-match-tips'}></div>
+        <div className={'no-match-tools'}>
+          <a className={'no-match-return'} href="#">回首页</a>
+        </div>
         <h1>这是一个 404 页面</h1>
       </div>
     );
