a0493a0bf55ae4c069d7853ef08cb7c07370f5a7
wanglili <wang508x102@163.com>
2016-08-03 16:59:33 +0800

    add no-login

--- src/containers/NoMatch/NoMatch.jsx
+++ src/containers/NoMatch/NoMatch.jsx
@@ -5,11 +5,10 @@ export default class NoMatch extends Component {
     return (
       <div className={'no-match'}>
         <div className={'no-match-logo'}></div>
-        <div className={'no-match-tips'}></div>
+        <div className={'no-match-tips'}>矮油~跑偏了~</div>
         <div className={'no-match-tools'}>
           <a className={'no-match-return'} href="#">回首页</a>
         </div>
-        <h1>这是一个 404 页面</h1>
       </div>
     );
   }
