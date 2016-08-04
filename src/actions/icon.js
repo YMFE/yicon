5eb18504ebd0bef0874ab00d4b27d68149dfb0b9
xilixinluo <xilixinluo@gmail.com>
2016-08-04 15:04:46 +0800

    .

--- src/actions/icon.js
+++ src/actions/icon.js
@@ -1,11 +1,11 @@
-import isonFetch from 'isom-fetch';
+import isomFetch from 'isom-fetch';
 import {
   FETCH_ICON_DETAIL,
   EDIT_ICON,
   EDIT_ICON_STYLE,
 } from '../constants/actionTypes';
 
-const fetch = isonFetch.create({ baseURL: '/api' });
+const fetch = isomFetch.create({ baseURL: '/api' });
 
 export function getIconDetail(iconId) {
   return {
@@ -27,3 +27,15 @@ export function editIconStyle(style) {
     payload: style,
   };
 }
+// export function downloadIcon(obj) {
+//   return (dispatch) => {
+//     dispatch({
+//       type: DOWNLOAD_ICONS,
+//       payload: fetch.post('/download/font', obj).then((response) => {
+//         if (response.res) {
+//           window.location.pathname = `/download/${response.data}`;
+//         }
+//       }),
+//     });
+//   };
+// }
