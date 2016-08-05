1a0fbb30247f7e119433eb5dea058dab21802a6f
malcolm <minghao_yu@163.com>
2016-08-05 14:45:26 +0800

    add prod

--- src/config.js
+++ src/config.js
@@ -18,7 +18,18 @@ const development = {
   },
   port: 3000,
 };
-const production = {};
+// 暂时线上也用这个
+const production = {
+  model: {
+    database: 'iconfont',
+    username: 'root',
+    password: 'odmMj7H6x2IaACrs',
+    dialect: 'mysql',
+    port: '3306',
+    host: '10.86.43.48',
+  },
+  port: 3000,
+};
 
 export default process.env.NODE_ENV !== 'production'
   ? { ...common, ...development }
